0d941c9b2f5b9fa16b2a43bb8a547bb0e004643f
adrian-dragos <adrian123795@gmail.com>
2023-04-10 12:38:32 +0300

    remove tab icon and remove default code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title '1AttemptFE'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('1AttemptFE');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
