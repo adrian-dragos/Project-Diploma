adb11de723810e440d3ad5779082a9625abace9e
adrian-dragos <adrian123795@gmail.com>
2023-05-17 15:29:39 +0300

    Add lessons table

--- FE/src/app/pages/lessons/lessons.component.ts
+++ FE/src/app/pages/lessons/lessons.component.ts
@@ -1,22 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { GetStudentLessonsListViewModel, LessonsClient } from '@api/api:';
+import { DatePipe } from '@angular/common';
 
 @Component({
 	selector: 'app-lessons',
 	templateUrl: './lessons.component.html',
 	styleUrls: ['./lessons.component.scss']
 })
-export class LessonsComponent {
-	displayedColumns: string[] = ['date', 'hour', 'name', 'instructor'];
+export class LessonsComponent implements OnInit {
+	displayedColumns: string[] = ['date', 'time', 'location', 'instructor'];
+	dataSource = new MatTableDataSource<GetStudentLessonsListViewModel>();
 
-	dataSource = new MatTableDataSource<any>(); // Replace `any` with your data type
+	constructor(private readonly lessonsClient: LessonsClient) {}
 
-	// You can set the data for your table in this component
-	ngOnInit() {
-		this.dataSource.data = [
-			{ name: 'John Doe', age: 25 },
-			{ name: 'Jane Smith', age: 32 }
-			// Add more data objects as needed
-		];
+	ngOnInit(): void {
+		this.lessonsClient.getStudentLessons(1).subscribe((lessons: GetStudentLessonsListViewModel[]) => {
+			this.dataSource.data = lessons;
+		});
 	}
 }
--- FE/src/app/pages/lessons/lessons.module.ts
+++ FE/src/app/pages/lessons/lessons.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LessonsComponent } from './lessons.component';
 import { MatTableModule } from '@angular/material/table';
+import { LessonsClient } from '@api/api:';
 
 @NgModule({
 	imports: [CommonModule, RouterModule.forChild([{ path: '', component: LessonsComponent }]), MatTableModule],
-	declarations: []
+	declarations: [],
+	providers: [LessonsClient]
 })
 export class LessonsModule {}
