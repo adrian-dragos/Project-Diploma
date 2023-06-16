80b38d9df06770836d45dfd39ed503856f96a0c0
adrian-dragos <adrian123795@gmail.com>
2023-06-16 13:44:09 +0300

    Remove unused code

--- BE/Infrastructure/Authorization/PermissionService.cs
+++ BE/Infrastructure/Authorization/PermissionService.cs
@@ -54,12 +54,6 @@ namespace Infrastructure.Authorization
               .Select(u => u.Role.Id)
               .FirstOrDefaultAsync();
 
-            var insturctors = await _dbContext.Set<Identity>()
-                .AsNoTracking()
-                .Where(x => x.Id == userId)
-                .Select(x => x.Students)
-                .FirstOrDefaultAsync();
-
             return await _dbContext.Set<PolicyRole>()
                 .AsNoTracking()
                 .Where(pr => pr.RoleId == userRoleId)
