4d02ffc0e402cdcef39dc4c31b87468730d31516
adrian-dragos <adrian123795@gmail.com>
2023-06-23 19:35:32 +0300

    add home page 2

--- FE/src/app/pages/presentation/home/home.module.ts
+++ FE/src/app/pages/presentation/home/home.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
-
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-	imports: [CommonModule, RouterModule.forChild([{ path: '', component: HomeComponent }])],
+	imports: [CommonModule, RouterModule.forChild([{ path: '', component: HomeComponent }]), MatButtonModule],
 	declarations: [HomeComponent]
 })
 export class HomeModule {}
