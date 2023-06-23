b4d0689e7a1588d5636c3782a98ebc13ec3e6d07
adrian-dragos <adrian123795@gmail.com>
2023-06-23 15:16:53 +0300

    update contacts page

--- FE/src/app/pages/presentation/contacts/contacts.component.ts
+++ FE/src/app/pages/presentation/contacts/contacts.component.ts
@@ -1,10 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 
 @Component({
-  selector: 'app-contacts',
-  templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.scss']
+	selector: 'app-contacts',
+	templateUrl: './contacts.component.html',
+	styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent {
+	apiLoaded: Observable<boolean>;
 
+	schoolLocation: google.maps.LatLngLiteral = {
+		lat: 45.746373685026256,
+		lng: 21.24781613107098
+	};
+	zoom = 15;
+	markerOptions: google.maps.MarkerOptions = { draggable: false };
+
+	mapOptions: google.maps.MapOptions = {
+		minZoom: 12,
+		maxZoom: 19,
+		streetViewControl: false
+	};
+
+	constructor(httpClient: HttpClient) {
+		this.apiLoaded = httpClient
+			.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyBAXM3kLgdcWR7xVtxGiB90kgB-8_DXAqI', 'callback')
+			.pipe(
+				tap((x) => console.log('Google Maps API loaded' + x)),
+				map(() => true),
+				catchError(() => of(false))
+			);
+	}
 }
--- FE/src/app/pages/presentation/contacts/contacts.module.ts
+++ FE/src/app/pages/presentation/contacts/contacts.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsComponent } from './contacts.component';
 import { RouterModule } from '@angular/router';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-	imports: [CommonModule, RouterModule.forChild([{ path: '', component: ContactsComponent }])],
+	imports: [CommonModule, RouterModule.forChild([{ path: '', component: ContactsComponent }]), GoogleMapsModule, MatIconModule],
 	declarations: [ContactsComponent]
 })
 export class ContactsModule {}
